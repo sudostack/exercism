fd9b08e54887a99d78c6609ae9116f626d8ea6b1
David Wu (sudostack) <dwu207@gmail.com>
2015-10-16 21:18:03 -0700

    Tests passing. Toughest problem was getting it to match letters.

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,12 +1,27 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-      input == "Tom-ay-to, tom-aaaah-to." ->
-         "Whatever."
-      input == "WATCH OUT!" ->
-        "Whoa, chill out!"
-      input ->
-        "#{input}"
+      no_peep?(input)           -> "Fine. Be that way!"
+      shouting?(input)          -> "Whoa, chill out!"
+      has_question_mark?(input) -> "Sure."
+      input                     -> "Whatever."
     end
+  end
+
+  defp no_peep?(input) do
+    String.strip(input) == ""
+  end
+
+  defp shouting?(input) do
+    input == String.upcase(input) && letters?(input)
+  end
+
+  defp has_question_mark?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp letters?(input) do
+    # Regex.match?(~r/[a-zA-Z]/, input)
+    Regex.match?(~r/\p{L}+/, input)
   end
 end
