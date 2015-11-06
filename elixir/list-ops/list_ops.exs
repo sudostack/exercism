60d36361a465bc240d558bd55af324e84fbf529c
sudostack <dwu207@gmail.com>
2015-11-06 00:20:35 -0800

    WIP: ListOps.count; 1/25 passing tests

---
+++ elixir/list-ops/list_ops.exs
@@ -0,0 +1,46 @@
+defmodule ListOps do
+  # Please don't use any external modules (especially List) in your
+  # implementation. The point of this exercise is to create these basic functions
+  # yourself.
+  #
+  # Note that `++` is a function from an external module (Kernel, which is
+  # automatically imported) and so shouldn't be used either.
+
+  @spec count(list) :: non_neg_integer
+  def count(l), do: sum(l)
+
+  defp sum(list, accum \\ 0) # this is a func head w/ no body (avoids confusion when using defaults)
+  defp sum([_|tail], accum), do: sum(tail, accum + 1)
+  defp sum([], accum), do: accum
+
+  @spec reverse(list) :: list
+  def reverse(l) do
+
+  end
+
+  @spec map(list, (any -> any)) :: list
+  def map(l, f) do
+
+  end
+
+  @spec filter(list, (any -> as_boolean(term))) :: list
+  def filter(l, f) do
+
+  end
+
+  @type acc :: any
+  @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
+  def reduce(l, acc, f) do
+
+  end
+
+  @spec append(list, list) :: list
+  def append(a, b) do
+
+  end
+
+  @spec concat([[any]]) :: [any]
+  def concat(ll) do
+
+  end
+end
