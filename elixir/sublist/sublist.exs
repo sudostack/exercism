58aec7156135f8d4a2892752efff7f1e9935d0a1
sudostack <dwu207@gmail.com>
2015-11-09 22:51:50 -0800

    WIP: Sublist.compare; 1/19 tests pass

---
+++ elixir/sublist/sublist.exs
@@ -0,0 +1,11 @@
+defmodule Sublist do
+  @doc """
+  Returns whether the first list is a sublist or a superlist of the second list
+  and if not whether it is equal or unequal to the second list.
+  """
+  def compare(a, b) do
+    cond do
+      a == b -> :equal
+    end
+  end
+end
