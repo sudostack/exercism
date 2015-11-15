563602ee2e807ae776b37dc0a123f2278e394887
David Wu (sudostack) <dwu207@gmail.com>
2015-11-14 23:41:44 -0800

    Sublist complete; inspired by another Alchemist's answer

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -5,36 +5,18 @@ defmodule Sublist do
   """
   def compare(a, b) do
     cond do
-      a == b                         -> :equal
-      Enum.empty?(a)                 -> :sublist
-      Enum.empty?(b)                 -> :superlist
-      comparator(a, b) == :sublist   -> :sublist
-      comparator(a, b) == :superlist -> :superlist
-      true                           -> :unequal
+      a === b        -> :equal
+      a === []       -> :sublist
+      b === []       -> :superlist
+      sublist?(a, b) -> :sublist
+      sublist?(b, a) -> :superlist
+      true           -> :unequal
     end
   end
 
-  defp comparator(list1, list2) do
-    list1_len = Enum.count(list1)
-    list2_len = Enum.count(list2)
-    list1_sum = Enum.sum(list1)
-    list2_sum = Enum.sum(list2)
-
-    cond do
-      (list1_len == list2_len) && (list1_sum == list2_sum) && same_num_type?(list1_sum, list2_sum) ->
-        :equal
-      (list1_len == list2_len) && (list1_sum < list2_sum) && same_num_type?(list1_sum, list2_sum) ->
-        :unequal
-      (list1_len < list2_len) && (list1_sum < list2_sum) && same_num_type?(list1_sum, list2_sum) ->
-        :sublist
-      (list1_len > list2_len) && (list1_sum > list2_sum) && same_num_type?(list1_sum, list2_sum) ->
-        :superlist
-      true ->
-        :unequal
-    end
-  end
-
-  defp same_num_type?(num1, num2) do
-    (is_float(num1) && is_float(num2)) || (!is_float(num1) && (!is_float(num2)))
+  defp sublist?(list1, list2) do
+    list2
+      |> Enum.chunk(length(list1), 1)
+      |> Enum.any?(fn list -> list === list1 end)
   end
 end
