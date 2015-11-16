3de171d59a15b09e2e2b0724c76a35675125e159
sudostack <dwu207@gmail.com>
2015-11-15 17:50:24 -0800

    WIP: DNA; 1/8 tests passing

---
+++ elixir/nucleotide-count/dna.exs
@@ -0,0 +1,34 @@
+defmodule DNA do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> DNA.count('AATAA', ?A)
+  4
+
+  iex> DNA.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  # the strand is entered as a "char list" (single quoted)
+  def count(strand, nucleotide) do
+    Enum.filter(strand, &(&1 === nucleotide)) |> Enum.count
+  end
+
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> DNA.nucleotide_counts('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec nucleotide_counts([char]) :: Dict.t
+  def nucleotide_counts(strand) do
+
+  end
+end
