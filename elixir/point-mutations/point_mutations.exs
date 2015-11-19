27eff8a59ab30df4849d576455cde429821c1784
sudostack <dwu207@gmail.com>
2015-11-18 21:41:57 -0800

    WIP: DNA.hamming_distance/2; 1/5 passing tests

---
+++ elixir/point-mutations/point_mutations.exs
@@ -0,0 +1,16 @@
+defmodule DNA do
+  @doc """
+  Returns number of differences between two strands of DNA, known as the Hamming Distance.
+
+  ## Examples
+
+  iex> DNA.hamming_distance('AAGTCATA', 'TAGCGATC')
+  4
+  """
+  @spec hamming_distance([char], [char]) :: non_neg_integer
+  def hamming_distance(strand1, strand2) do
+    ham(strand1, strand2)
+  end
+
+  defp ham('', ''), do: 0
+end
