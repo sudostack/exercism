916830d6f936751440b3eed8e5e24da3eff72269
sudostack <dwu207@gmail.com>
2015-11-18 22:22:09 -0800

    DNA.hamming_distance/2 complete

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -9,9 +9,17 @@ defmodule DNA do
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
   def hamming_distance(strand1, strand2) do
-    ham(strand1, strand2)
+    compare(strand1, strand2)
   end
 
-  defp ham('', ''), do: 0
-  defp ham(strand, strand), do: 0
+  defp compare(a, b, acc \\ 0)
+  defp compare([], [], acc), do: acc
+  defp compare([], _, _), do: nil
+  defp compare(_, [], _), do: nil
+  defp compare([head1|tail1], [head2|tail2], acc) do
+    cond do
+      head1 === head2 -> compare(tail1, tail2, acc)
+      head1 !== head2 -> compare(tail1, tail2, acc + 1)
+    end
+  end
 end
