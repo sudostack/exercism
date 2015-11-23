500c6ccf9cf244c6492d4f32f34c4d2f5645ffa4
sudostack <dwu207@gmail.com>
2015-11-22 21:44:24 -0800

    WIP: Meetup.meetup/4; building calendar matrix

--- elixir/meetup/meetup.exs
+++ elixir/meetup/meetup.exs
@@ -17,6 +17,42 @@ defmodule Meetup do
   """
   @spec meetup(pos_integer, pos_integer, weekday, schedule) :: :calendar.date
   def meetup(year, month, weekday, schedule) do
-    {2013, 5, 13}
+    # {2013, 5, 13}
+    # go through each calendar day until :calendar.last_day_of_the_month(year, month)
+      # check if weekday(day_of_the_week/3) matches `weekday`
+      # check if
+  end
+
+  # defp cal_matrix(year, month) do
+  #   matrix = [
+  #     [], # week 1
+  #     [],
+  #     [],
+  #     [],
+  #     []
+  #   ]
+
+  #   first_dotw = :calendar.day_of_the_week({year, month, 1}) - 1
+  #   last_dotm  = :calendar.last_day_of_the_month(year, month)
+
+  #   Enum.each 1..last_dotm, fn day do
+  #     cond do
+  #       day == 1 ->
+  #       true     ->
+  #     end
+  #   end
+  # end
+
+  defp weekday(day) do
+    %{
+      :monday    => 1,
+      :tuesday   => 2,
+      :wednesday => 3,
+      :thursday  => 4,
+      :friday    => 5,
+      :saturday  => 6,
+      :sunday    => 7
+    }
+    |> Map.get(day)
   end
 end
