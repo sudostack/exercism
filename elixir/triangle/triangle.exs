bcb5ddbac4a40491d46cacdeaed57ada6597592f
sudostack <dwu207@gmail.com>
2015-12-03 01:17:18 -0800

    WIP: Triangle.kind/3; 1/15 tests passing

---
+++ elixir/triangle/triangle.exs
@@ -0,0 +1,11 @@
+defmodule Triangle do
+  @type kind :: :equilateral | :isosceles | :scalene
+
+  @doc """
+  Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
+  """
+  @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }
+  def kind(a, a, a), do: {:ok, :equilateral}
+
+  # def equilateral?(a, a, a), do: true
+end
