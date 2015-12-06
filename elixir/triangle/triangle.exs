050bdbfc0b02db80d6f71957b59cff4b024f3de2
David Wu (sudostack) <dwu207@gmail.com>
2015-12-06 07:43:29 -0800

    refactor: Triangle.kind/3; use list and sort

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -5,22 +5,14 @@ defmodule Triangle do
   Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
   """
   @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }
-  def kind(a, b, c) when a < 0 or b < 0 or c < 0 do
-    {:error, "all side lengths must be positive"}
+  def kind(a, b, c) do
+    case [a, b, c] |> Enum.sort do
+      [a, _, _] when a <= 0     -> {:error, "all side lengths must be positive"}
+      [a, b, c] when c >= a + b -> {:error, "side lengths violate triangle inequality"}
+      [a, a, a]                 -> {:ok, :equilateral}
+      [a, a, _]                 -> {:ok, :isosceles}
+      [_, b, b]                 -> {:ok, :isosceles}
+      [_, _, _]                 -> {:ok, :scalene}
+    end
   end
-  def kind(0, 0, 0),    do: {:error, "all side lengths must be positive"}
-  def kind(a, a, a),    do: {:ok, :equilateral}
-  def kind(a, a, b) when a < b do
-    {:error, "side lengths violate triangle inequality"}
-  end
-  def kind(a, b, a) when a < b do
-    {:error, "side lengths violate triangle inequality"}
-  end
-  def kind(a, b, b) when b < a do
-    {:error, "side lengths violate triangle inequality"}
-  end
-  def kind(a, a, _),    do: {:ok, :isosceles}
-  def kind(_, a, a),    do: {:ok, :isosceles}
-  def kind(a, _, a),    do: {:ok, :isosceles}
-  def kind(_a, _b, _c), do: {:ok, :scalene}
 end
