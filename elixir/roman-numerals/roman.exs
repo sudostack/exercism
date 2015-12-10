19711b7ab51ec368406fe4d288089023b3ee357c
sudostack <dwu207@gmail.com>
2015-12-10 00:01:24 -0800

    WIP: Roman.numerals/1; 6/18 tests passing

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -5,8 +5,14 @@ defmodule Roman do
   @spec numerals(pos_integer) :: String.t
   def numerals(number) do
     cond do
-      number < 5 ->
+      number < 4 ->
         Enum.reduce 1..number, "", fn num, acc -> acc <> "I" end
+      number < 5 ->
+        numerals(5 - number) <> "V"
+      number == 5 ->
+        "V"
+      number > 5 and number < 10 ->
+        "V" <> numerals(number - 5)
     end
   end
 end
--- elixir/roman-numerals/roman_numerals_test.exs
+++ elixir/roman-numerals/roman_numerals_test.exs
@@ -30,17 +30,17 @@ defmodule RomanTest do
     assert Roman.numerals(4) == "IV"
   end
 
-  @tag :pending
+  # @tag :pending
   test "5" do
     assert Roman.numerals(5) == "V"
   end
 
-  @tag :pending
+  # @tag :pending
   test "6" do
     assert Roman.numerals(6) == "VI"
   end
 
-  @tag :pending
+  # @tag :pending
   test "9" do
     assert Roman.numerals(9) == "IX"
   end
