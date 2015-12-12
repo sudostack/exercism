83fff829468fa0520f3c93db13c70625e7ef7e55
David Wu (sudostack) <dwu207@gmail.com>
2015-12-12 09:27:31 -0800

    WIP: Roman.numerals/1; fixed roman_concat/3

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -1,3 +1,5 @@
+require IEx
+
 defmodule Roman do
   @doc """
   Convert the number to a roman number.
@@ -5,61 +7,77 @@ defmodule Roman do
   @spec numerals(pos_integer) :: String.t
   def numerals(number) do
     { number, 0 }
-    |> thousands
-    |> five_hunds
-    |> centrums
-    |> fifties
-    |> tens
-    |> fives
+    # |> thousands
+    # |> five_hunds
+    # |> centrums
+    # |> fifties
+    # |> tens
+    # |> fives
     |> ones
     |> elem(1)
-
-    defp roman_concat(char, multiplier, str \\ "") do
-      Enum.reduce 1..multiplier, str, fn(char, acc) ->
-        acc <> char
-      end
-    end
-
-    defp remainder(num_tup, divisor) do
-      rem(elem(num_tup, 0), divisor)
-    end
-
-    defp thousands(num_tup) do
-      { remainder(num_tup, 1000), roman_concat("M", elem(num_tup, 0)/1000) }
-    end
-
-    defp five_hunds(num_tup) do
-      { remainder(num_tup, 500), roman_concat("D", elem(num_tup, 0)/500) }
-    end
-
-    defp centrums(num_tup) do
-      { remainder(num_tup, 100), roman_concat("C", elem(num_tup, 0)/100) }
-    end
-
-    defp fifties(num_tup) do
-      { remainder(num_tup, 50), roman_concat("L", elem(num_tup, 0)/50) }
-    end
-
-    defp tens(num_tup) do
-      { remainder(num_tup, 10), roman_concat("L", elem(num_tup, 0)/10) }
-    end
-
-    defp fives(num_tup) do
-      { remainder(num_tup, 5), roman_concat("V", elem(num_tup, 0)/5) }
-    end
-
-    defp ones(num_tup) do
-      { 0, roman_concat("I", elem(num_tup, 0)/1) }
-    end
-    # cond do
-    #   number < 4 ->
-    #     Enum.reduce 1..number, "", fn num, acc -> acc <> "I" end
-    #   number < 5 ->
-    #     numerals(5 - number) <> "V"
-    #   number == 5 ->
-    #     "V"
-    #   number > 5 and number < 10 ->
-    #     "V" <> numerals(number - 5)
-    # end
   end
+
+  def roman_concat(char, multiplier, str \\ "") do
+    Enum.reduce 1..multiplier, str, fn(_count, acc) ->
+      acc <> char
+    end
+  end
+
+  def remainder(num_tup, divisor) do
+    { num, div } = num_tup
+    multiples    = multiplier(num_tup, divisor)
+
+    num - (divisor * multiples)
+  end
+
+  def multiplier(num_tup, divisor) do
+    { num, _str } = num_tup
+
+    num / divisor |> Float.floor |> round
+  end
+
+  # 800 => DCCC
+  # 900 => CM vs DCCCC
+
+  # 8 => VIII vs IIX
+  # 9 => VIIII vs IX
+
+  def thousands(num_tup) do
+    { remainder(num_tup, 1000), roman_concat("M", multiplier(num_tup, 1000)) }
+  end
+
+  def five_hunds(num_tup) do
+    { remainder(num_tup, 500), roman_concat("D", multiplier(num_tup, 500)) }
+  end
+
+  def centrums(num_tup) do
+    { remainder(num_tup, 100), roman_concat("C", multiplier(num_tup, 100)) }
+  end
+
+  def fifties(num_tup) do
+    { remainder(num_tup, 50), roman_concat("L", multiplier(num_tup, 50)) }
+  end
+
+  def tens(num_tup) do
+    { remainder(num_tup, 10), roman_concat("X", multiplier(num_tup, 10)) }
+  end
+
+  def fives(num_tup) do
+    { remainder(num_tup, 5), roman_concat("V", multiplier(num_tup, 5)) }
+  end
+
+  def ones(num_tup) do
+    { remainder(num_tup, 1), roman_concat("I", multiplier(num_tup, 1)) }
+  end
+
+  # cond do
+  #   number < 4 ->
+  #     Enum.reduce 1..number, "", fn num, acc -> acc <> "I" end
+  #   number < 5 ->
+  #     numerals(5 - number) <> "V"
+  #   number == 5 ->
+  #     "V"
+  #   number > 5 and number < 10 ->
+  #     "V" <> numerals(number - 5)
+  # end
 end
