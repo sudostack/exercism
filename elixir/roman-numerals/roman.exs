555c8065b5c04918a239a6d8be3d38d448f66d68
sudostack <dwu207@gmail.com>
2015-12-17 07:52:24 -0800

    WIP: Roman.numerals/1; new direction

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -3,18 +3,20 @@ require IEx
 defmodule Roman do
   @doc """
   Convert the number to a roman number.
+
+  APPROACH:
+  800 => DCCC
+  900 => CM vs DCCCC
+
+  8 => VIII vs IIX
+  9 => VIIII vs IX
+
+  Figure out if the next denomination up, minus the current denom is equal to target (when remainder is zero)
+  If so, take it, if not, then start w/ the current denom and build
   """
   @spec numerals(pos_integer) :: String.t
   def numerals(number) do
-    { number, 0 }
-    # |> thousands
-    # |> five_hunds
-    # |> centrums
-    # |> fifties
-    # |> tens
-    # |> fives
-    |> ones
-    |> elem(1)
+    deduction({ number, :thousands })
   end
 
   @denom_order ~w(thousands five_hunds centrums fifties tens fives)
@@ -28,93 +30,24 @@ defmodule Roman do
     ones:       { 1, "I" }
   }
 
+  def deduction({ count, denomination }, acc_str \\ "") when count / Map.get(@denominations, denomination) > 0 do
+    { divisor, char } = Map.get(@denominations, denomination)
+    times = (count / divisor) |> Float.floor |> round
+    { count - (divisor * times), roman_concat(char, times) }
+  end
+  def deduction({ count, denomination }, acc_str) when count > 0 do
+
+  end
+  def deduction({ count, denomination }, acc_str), do: acc_str
+
+  def next_denom(denom) when denom != :thousands do
+    Enum.find_index(@denominations, denom) - 1
+  end
+  def next_denom(denom) when denom == :thousands, do: nil
+
   def roman_concat(char, multiplier, str \\ "") do
     Enum.reduce 1..multiplier, str, fn(_count, acc) ->
       acc <> char
     end
   end
-
-  def remainder(num_tup, divisor) do
-    multiples = multiplier(num_tup, divisor)
-
-    num - (divisor * multiples)
-  end
-
-  def multiplier(num_tup, divisor) do
-    { num, _str } = num_tup
-
-    num / divisor |> Float.floor |> round
-  end
-
-  # 800 => DCCC
-  # 900 => CM vs DCCCC
-
-  # 8 => VIII vs IIX
-  # 9 => VIIII vs IX
-
-  # Figure out if the next denomination up, minus the current denom is equal to target
-  # If so, take it, if not, then start w/ the current denom and build
-
-  def next?(count) do
-
-  end
-
-  def convert(0, acc_str), do: acc_str
-  def convert(count, acc_str) do
-
-  end
-
-  def thousands(num_tup) do
-    { remainder(num_tup, 1000), roman_concat("M", multiplier(num_tup, 1000)) }
-  end
-
-  def five_hunds(num_tup) do
-    { remainder(num_tup, 500), roman_concat("D", multiplier(num_tup, 500)) }
-  end
-
-  def centrums(num_tup) do
-    { remainder(num_tup, 100), roman_concat("C", multiplier(num_tup, 100)) }
-  end
-
-  def fifties(num_tup) do
-    { remainder(num_tup, 50), roman_concat("L", multiplier(num_tup, 50)) }
-  end
-
-  def under_ten(num_tup) do
-    cond do
-      number < 4 ->
-        Enum.reduce 1..number, "", fn num, acc -> acc <> "I" end
-      number < 5 ->
-        numerals(5 - number) <> "V"
-      number == 5 ->
-        "V"
-      number > 5 and number < 9 ->
-        "V" <> numerals(number - 5)
-      number < 10 ->
-        numerals(10 - number) <> "X"
-    end
-  end
-
-  def tens(num_tup) do
-    { remainder(num_tup, 10), roman_concat("X", multiplier(num_tup, 10)) }
-  end
-
-  def fives(num_tup) do
-    { remainder(num_tup, 5), roman_concat("V", multiplier(num_tup, 5)) }
-  end
-
-  def ones(num_tup) do
-    { remainder(num_tup, 1), roman_concat("I", multiplier(num_tup, 1)) }
-  end
-
-  # cond do
-  #   number < 4 ->
-  #     Enum.reduce 1..number, "", fn num, acc -> acc <> "I" end
-  #   number < 5 ->
-  #     numerals(5 - number) <> "V"
-  #   number == 5 ->
-  #     "V"
-  #   number > 5 and number < 10 ->
-  #     "V" <> numerals(number - 5)
-  # end
 end
