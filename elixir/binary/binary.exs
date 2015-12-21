a2b66a489757b526259eded446b8b5f04cbdbfd6
David Wu (sudostack) <dwu207@gmail.com>
2015-12-20 23:19:49 -0800

    completed: Binary.to_decimal/1

--- elixir/binary/binary.exs
+++ elixir/binary/binary.exs
@@ -6,12 +6,17 @@ defmodule Binary do
   """
   @spec to_decimal(String.t) :: non_neg_integer
   def to_decimal(string) do
-    string
-    |> String.split("", trim: true)
-    |> to_int
+    cond do
+      Regex.scan(~r/[01]/, string) |> length == String.length(string) ->
+        convert(string)
+      true ->
+        0
+    end
   end
 
-  defp to_int(num_list) do
+  defp convert(bin_string) do
+    num_list = bin_string |> String.split("", trim: true)
+
     Enum.zip(num_list, (length(num_list) - 1)..0)
     |> Enum.reduce 0, fn { multiplier, pow }, sum ->
       sum + (String.to_integer(multiplier) * :math.pow(2, pow))
