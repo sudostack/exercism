931e09504824ed085cd4d3b34e240ce55adbe33c
David Wu (sudostack) <dwu207@gmail.com>
2015-12-21 20:21:44 -0800

    completed: PrimeFactors.factors_for/1

--- elixir/prime-factors/prime_factors.exs
+++ elixir/prime-factors/prime_factors.exs
@@ -8,19 +8,12 @@ defmodule PrimeFactors do
   The prime factors of 'number' will be ordered lowest to highest.
   """
   @spec factors_for(pos_integer) :: [pos_integer]
-  def factors_for(number) do
-    deduce(number)
-  end
+  def factors_for(number), do: deduce(number)
 
-  def deduce(_number, divisor \\ 2, factors \\ [])
-  def deduce(1, _divisor, factors) do
-    factors
-  end
-  def deduce(number, divisor, factors) when rem(number, divisor) == 0 do
-    # passing a float introduces infinite loop; use `div` instead
+  defp deduce(_number, divisor \\ 2, factors \\ [])
+  defp deduce(1, _divisor, factors), do: factors
+  defp deduce(number, divisor, factors) when rem(number, divisor) == 0 do
     deduce(div(number, divisor), divisor, factors ++ [divisor])
   end
-  def deduce(number, divisor, factors) do
-    deduce(number, divisor + 1, factors)
-  end
+  defp deduce(number, divisor, factors), do: deduce(number, divisor + 1, factors)
 end
