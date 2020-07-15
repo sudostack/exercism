45b963f46cf4cd8166cd1504664ef537288c7de3
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-07-15 14:27:08 -0700

    rust - 2. leap year : refactor based on community solutions

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,7 +1,6 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if div_by(4, year) && !div_by(100, year) && !div_by(400, year) {
-        true
-    } else { div_by(4, year) && div_by(100, year) && div_by(400, year) }
+    div_by(4, year) && div_by(100, year) && div_by(400, year)
+        || div_by(4, year) && !div_by(100, year) && !div_by(400, year)
 }
 
 fn div_by(num: u64, year: u64) -> bool {
