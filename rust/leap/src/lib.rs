1f246608306d271bcf28e6225868d62a63610038
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-07-15 14:22:11 -0700

    rust - 2. leap year : refactor based on clippy suggestions

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,13 +1,9 @@
 pub fn is_leap_year(year: u64) -> bool {
     if div_by(4, year) && !div_by(100, year) && !div_by(400, year) {
-        return true;
-    } else if div_by(4, year) && div_by(100, year) && div_by(400, year) {
-        return true;
-    } else {
-        return false;
-    }
+        true
+    } else { div_by(4, year) && div_by(100, year) && div_by(400, year) }
 }
 
 fn div_by(num: u64, year: u64) -> bool {
-    return year % num == 0;
+    year % num == 0
 }
